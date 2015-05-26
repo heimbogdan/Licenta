069db3c82a22c1ec5495922dfa6b322730b7bace
Bogdan Heim <heim.bogdan@hotmail.com>
2015-05-26 09:58:02 +0300

    asd

---
+++ Test/JUnitTests/bh/guillotine/entity/element/EntityElementAllTests.java
@@ -0,0 +1,12 @@
+package bh.guillotine.entity.element;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestElement.class, TestElementList.class,
+		TestFinalElement.class, TestGeneralElement.class })
+public class EntityElementAllTests {
+
+}
